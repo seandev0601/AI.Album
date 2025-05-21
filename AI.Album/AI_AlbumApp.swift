bcaf0ba4d809f7ef1fa2dd1f9a8d0f132dc0cf90
Sean <seanbopo@gmail.com>
2025-05-21 22:31:05 +0800

    Initial Commit

---
+++ AI.Album/AI_AlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_AlbumApp.swift
+//  AI.Album
+//
+//  Created by Po Hsiang Chao on 2025/5/21.
+//
+
+import SwiftUI
+
+@main
+struct AI_AlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI.Album/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI.Album
+//
+//  Created by Po Hsiang Chao on 2025/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
